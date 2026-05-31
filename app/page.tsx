bdd6c13f35c08f7cfc2879aaa81b17b45cb9a20e
Bhavmanyu <bhavmanyu25@gmail.com>
2026-05-31 10:16:32 +0530

    new edits to pages

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from "@/components/home/HeroSection";
 import FeaturedTransformations from "@/components/home/FeaturedTransformations";
 import SignaturePackages from "@/components/home/SignaturePackages";
 import WhyRoope from "@/components/home/WhyRoope";
+import ArtistSpotlight from "@/components/home/ArtistSpotlight";
 import TrustSection from "@/components/home/TrustSection";
 import ReviewsSection from "@/components/home/ReviewsSection";
 import OffersSection from "@/components/home/OffersSection";
@@ -14,6 +15,7 @@ export default function Home() {
       <FeaturedTransformations />
       <SignaturePackages />
       <WhyRoope />
+      <ArtistSpotlight />
       <TrustSection />
       <OffersSection />
       <ReviewsSection />
